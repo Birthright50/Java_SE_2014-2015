7e6e958778bab5565e4e1375ee5bf67a6519e0de
Birthright <birthright5050@gmail.com>
2014-09-14 00:27:57 +0400

    task003

---
+++ TASK003.java
@@ -0,0 +1,16 @@
+/**
+
+ *
+ *
+ */
+
+import java.util.Scanner;
+
+public class TASK003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int R = 3;
+        double S = (4 * Math.PI * Math.pow(R, 3)) / 3;
+        System.out.println("Объем равен = " + S);
+    }
+}
