4fa4e88d5352840bb9bc628edd76f0629f6f1c57
Birthright <birthright5050@gmail.com>
2014-09-14 00:31:07 +0400

    task008

---
+++ TASK008.java
@@ -0,0 +1,29 @@
+/**
+ * Created by BirthrightL on 13.09.2014.
+ */
+
+import java.util.Scanner;
+
+public class TASK008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = 7;
+        int a = (2 * k);
+        int b = (3 * k);
+        int c = (4 * k);
+        int d = (5 * k);
+        int e = (6 * k);
+        int f = (7 * k);
+        int i = (8 * k);
+        int g = (9 * k);
+        System.out.println("2*k=" + a);
+        System.out.println("3*k=" + b);
+        System.out.println("4*k=" + c);
+        System.out.println("5*k=" + d);
+        System.out.println("6*k=" + e);
+        System.out.println("7*k=" + f);
+        System.out.println("8*k=" + i);
+        System.out.println("9*k=" + g);
+
+    }
+}
