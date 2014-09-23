ef1c8c6fed1b9273d74ae2680ef5338ba11633a2
Birthright <birthright5050@gmail.com>
2014-09-23 21:08:54 +0400

    task010 added

---
+++ task/src/TASK010.java
@@ -0,0 +1,27 @@
+/**
+ * Created by BirthrightL on 23.09.2014.
+ */
+
+import java.util.Scanner;
+
+
+public class TASK010 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Ввод х");
+        double x = sc.nextDouble();
+        double y = 0;
+        if (x > 2) {
+            y = (x * x - 1) / (x + 2);
+        }
+        if (x > 0 && x <= 2) {
+            y = (x * x - 1) * (x + 2);
+        }
+        if (x <= 0) {
+            y = x * x * (1 + 2 * x);
+        }
+        System.out.println(y);
+    }
+}
+
