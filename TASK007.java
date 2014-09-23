0f1fd93292f8622ca5bb570e5e77060bb67d9512
Birthright <birthright5050@gmail.com>
2014-09-23 21:04:23 +0400

    task007 updated

--- TASK007.java
+++ TASK007.java
@@ -1,29 +1,25 @@
 /**
- * Created by BirthrightL on 13.09.2014.
+ * Created by BirthrightL on 23.09.2014.
  */
 
 import java.util.Scanner;
 
 public class TASK007 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = 5;
-        int b = 7;
-        double c = a + b;
-        double d = a - b;
-        double e = b - a;
-        double f = a * b;
-        double g = a / b;
-        double h = a % b;
-        double i = b / a;
-        double j = b % a;
-        System.out.println("a+b = " + c);
-        System.out.println("a-b=" + d);
-        System.out.println("b-a=" + e);
-        System.out.println("a*b=" + f);
-        System.out.println("a/b=" + g);
-        System.out.println("a%b=" + h);
-        System.out.println("b/a=" + i);
-        System.out.println("b%a=" + j);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ввод a");
+        int a = scanner.nextInt();
+        System.out.println("ввод b");
+        int b = scanner.nextInt();
+        System.out.println("a + b =" + (a + b));
+        System.out.println("a - b =" + (a - b));
+        System.out.println("b - a =" + (b - a));
+        System.out.println("a * b =" + (a * b));
+        System.out.println("a / b =" + (a / b));
+        System.out.println("a % b =" + (a % b));
+        System.out.println("b / a =" + (b / a));
+        System.out.println("b % a =" + (b % a));
     }
+
 }
