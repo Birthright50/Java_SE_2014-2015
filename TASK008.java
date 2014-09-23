fa55eca2eaf98ba418bc3dc3809e7ccff25fa5d1
Birthright <birthright5050@gmail.com>
2014-09-23 21:04:57 +0400

    task008 corrected

--- TASK008.java
+++ TASK008.java
@@ -1,29 +1,19 @@
 /**
- * Created by BirthrightL on 13.09.2014.
+ * Created by BirthrightL on 23.09.2014.
  */
 
 import java.util.Scanner;
 
 public class TASK008 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int k = 7;
-        int a = (2 * k);
-        int b = (3 * k);
-        int c = (4 * k);
-        int d = (5 * k);
-        int e = (6 * k);
-        int f = (7 * k);
-        int i = (8 * k);
-        int g = (9 * k);
-        System.out.println("2*k=" + a);
-        System.out.println("3*k=" + b);
-        System.out.println("4*k=" + c);
-        System.out.println("5*k=" + d);
-        System.out.println("6*k=" + e);
-        System.out.println("7*k=" + f);
-        System.out.println("8*k=" + i);
-        System.out.println("9*k=" + g);
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ввод k");
+        int k = scanner.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " x " + k + " = " + (i * k));
+        }
     }
+
 }
+
