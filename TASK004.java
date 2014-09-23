449d2577d12c9512a19a11e0c0e4664490699792
Birthright <birthright5050@gmail.com>
2014-09-23 21:01:55 +0400

    task004 corrected

--- TASK004.java
+++ TASK004.java
@@ -1,17 +1,32 @@
 /**
- * Created by BirthrightL on 13.09.2014.
+ * Created by BirthrightL on 23.09.2014.
  */
 
 import java.util.Scanner;
 
 public class TASK004 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double x = 5;
-        double y = 6.3;
-        double z = ((1 + y) * (2 * x + Math.pow(y, 2) - (x + y)) / (y + 1) / Math.pow(x, 2) - 4);
-        System.out.println(z);
+        double x = 3;
+        double y = 1;
+        double z = 0.0;
+        double v = 0.0;
+        double s = 0.0;
+        z = x * x - 4;
+        z = 1 / z;
+        v = y + z;
+        s = x + y;
+        s = s / v;
+        z = 2 * x;
+        v = y * y;
+        z = z + v;
+        z = z - s;
+        s = 1 + y;
+        v = z * s;
+
+
+        System.out.println(v);
     }
 
 
 }
+
