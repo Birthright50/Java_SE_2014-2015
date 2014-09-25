7c524458b151655b5339e2268ec8627cd5fd9a60
Birthright <birthright5050@gmail.com>
2014-09-25 22:40:50 +0400

    penalty task task999 added

---
+++ task/src/TASK999.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/**
+ * Created by BirthrightL on 25.09.2014.
+ */
+public class TASK999 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Ввод х");
+        double x = sc.nextDouble();
+        System.out.println("Ввод у");
+        double y = sc.nextDouble();
+        System.out.println("Ввод z");
+        double z = sc.nextDouble();
+        double a;
+        double b;
+        double c;
+        double d;
+        a = x + y;
+        b = 2 - x;
+        a = a * b;
+        b = x + 687;
+        c = 100 * y;
+        b = b + c;
+        a = a / b;
+        b = 54 * z;
+        b = x + b;
+        c = y - 2;
+        c = y * z;
+        c = c - 365;
+        b = b / c;
+        c = 258 * z;
+        b = b / c;
+        a = a - b;
+        b = x + y;
+        c = 2 - x;
+        b = b * c;
+        c = y + 607;
+        d = 100 * x;
+        c = c + d;
+        b = b / c;
+        a = a / b;
+        b = 73 * z;
+        b = b + x;
+        c = x - 2;
+        c = c * y;
+        c = c - 365;
+        b = b / c;
+        c = 213 * z;
+        b = b / c;
+        c = 5021 * z;
+        b = b * c;
+        a = a - b;
+        System.out.println(a);
+
+
+    }
+}
