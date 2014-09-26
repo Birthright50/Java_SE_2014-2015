2842060efad2b78845eeae0449abc59af60b9e45
Birthright <birthright5050@gmail.com>
2014-09-26 12:27:14 +0400

    task006 corrected

---
+++ task/src/TASK006.java
@@ -0,0 +1,19 @@
+/**
+ * Created by BirthrightL on 23.09.2014.
+ */
+
+import java.util.Scanner;
+
+public class TASK006 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = 1;
+        int z;
+        z = x + 6;
+        z = x * z + 10;
+        z = x * z + 25;
+        z = x * z + 30;
+        z = x * z + 101;
+        System.out.println(z);
+    }
+}
