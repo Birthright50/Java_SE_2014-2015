97902baec2d62a5e0a9c8371f090d1253622022a
Birthright <birthright5050@gmail.com>
2014-09-28 13:25:36 +0400

    task024 corrected

--- task/src/TASK024.java
+++ task/src/TASK024.java
@@ -10,21 +10,19 @@ public class TASK024 {
         final double EPS = 1e-9;
         System.out.println("Ввод х");
         double x = sc.nextDouble();
-        double numerator = 1;
         double denominator = 9 * (x - 1) * (x - 1);
         double s = 0;
-        double z=1;
-        double b;
-        if (x==1)
+        double z = 1;
+        if (x == 1)
             System.out.println("Infinity");
         else {
-        for (int i = 2; z > EPS; i++) {
-            s = s + numerator / denominator;
-            z = numerator / denominator;
-            b = i * 9;
-            denominator = denominator * b * (x - 1) * (x - 1) / (i - 1);
-               }
-        System.out.println(s); }
+            for (int i = 2; z > EPS; i++) {
+                s = s + 1 / denominator;
+                z = 1 / denominator;
+                denominator = denominator * i * 9 * (x - 1) * (x - 1) / (i - 1);
+            }
+            System.out.println(s);
+        }
 
     }
 }
