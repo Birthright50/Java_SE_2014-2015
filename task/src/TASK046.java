f15d72f9d9470a97bb1121e93c8339824009a00e
Birthright <birthright5050@gmail.com>
2014-10-31 18:07:15 +0400

    task046 corrected

--- task/src/TASK046.java
+++ task/src/TASK046.java
@@ -4,21 +4,46 @@ import java.util.Scanner;
  * Created by BirthrightL on 29.10.2014.
  */
 public class TASK046 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
         String b = sc.nextLine();
         a = a.toLowerCase();
         b = b.toLowerCase();
-        if (a.compareToIgnoreCase(b) > 0) {
-            System.out.println("Вторая");
+        if (a.equals(b)) {
+            System.out.println("Одинаковые строки");
+        }
+        int c = a.length();
+        int d = b.length();
+        int min;
+        if (c <= d) {
+            min = c;
         } else {
-            if (a.compareToIgnoreCase(b) < 0) {
+            min = d;
+        }
+        char e[] = a.toCharArray();
+        char f[] = b.toCharArray();
+        for (int i = 0; i < min; i++) {
+            char c1 = e[i];
+            char c2 = f[i];
+            if (c1 < c2) {
                 System.out.println("Первая");
-            } else {
-                System.out.println("Одинаковые строки");
+                return;
+            }
+            if (c1 > c2) {
+                System.out.println("Вторая");
+                return;
             }
         }
+        if (c <= d && a.equals(b.substring(0, min))){
+            System.out.println("Первая");
+        }
+        if (d < c && b.equals(a.substring(0, min))){
+            System.out.println("Вторая");
+        }
+
+
     }
 }
 
