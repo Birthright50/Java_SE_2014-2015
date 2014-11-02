97bf2338980ad9e38be732bd9917a17358f24858
Birthright <birthright5050@gmail.com>
2014-11-03 01:39:27 +0400

    task032 corrected

--- task/src/TASK032.java
+++ task/src/TASK032.java
@@ -6,43 +6,35 @@ import java.util.Scanner;
 public class TASK032 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ввод кол-во элементов первого массива");
         int q = sc.nextInt();
         int[] a = new int[q];
-        System.out.println("Ввод кол-во элементов второго массива");
         int j = sc.nextInt();
         int[] b = new int[j];
-        System.out.println("Ввод элементов первого массива");
-        String str = "";
         for (int i = 0; i < q; i++) {
             a[i] = sc.nextInt();
-            String str1 = Integer.toString(a[i]);
-            str = str + str1;
         }
-        long m = Long.parseLong(str);
-        System.out.println("m="+m);
-
-        str = "";
-        System.out.println("Ввод элементов второго массива");
         for (int i = 0; i < j; i++) {
             b[i] = sc.nextInt();
-            String str1 = Integer.toString(b[i]);
-            str = str + str1;
         }
-        long n = Long.parseLong(str);
-        System.out.println("n="+n);
-        System.out.println("m+n="+(m + n));
+        if (q >= j) {
+            for (int i = 0; i < j; i++) {
+                a[i + q - j] = a[i + q - j] + b[i];
+            }
+            for (int i = 0; i < q; i++) {
+                System.out.print(a[i] + " ");
+            }
+        } else {
+            for (int i = 0; i < q; i++) {
+                b[i + j - q] = b[i + j - q] + a[i];
+            }
+            for (int i = 0; i < j; i++) {
+                System.out.print(b[i] + " ");
+
+
+            }
+        }
     }
-
-
 }
-
-
-
-
-
-
-
 
 
 
