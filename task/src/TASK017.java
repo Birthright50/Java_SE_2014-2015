c55c2314a96cdd8b468eb80cc996c46107200960
Birthright <birthright5050@gmail.com>
2014-11-09 01:29:26 +0400

    task017 corrected

--- task/src/TASK017.java
+++ task/src/TASK017.java
@@ -5,27 +5,17 @@
 import java.util.Scanner;
 
 public class TASK017 {
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         double s = 0.5;
         double fact1 = 1;
-        double fact2 = 6;
-        if (n <= 0)
-            System.out.println("! n>=1");
-        else {
-
-                    for (int i = 2; i <= n; i++) {
-                        fact1 = fact1 * (i - 1);
-                        fact2 = fact2 * 2 * i;
-                        s = s + ((fact1 * fact1) / fact2);
-                        fact2=fact2*(i+3);
-
-                    }
-                System.out.println(s);
-            }
-
-
+        double fact2 = 24;
+        for (int i = 2; i <= n; i++) {
+            fact1 = fact1 * (i - 1);
+            s = s + ((fact1 * fact1) / fact2);
+            fact2 = fact2 * (2 * i + 1) * (2 * i + 2);
         }
+        System.out.println(s);
     }
+}
