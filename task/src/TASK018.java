7fea1b587fd44c0acd9fda7ca830f328f027816d
Birthright <birthright5050@gmail.com>
2014-11-10 01:31:28 +0400

    task018 corrected

--- task/src/TASK018.java
+++ task/src/TASK018.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
  * Created by BirthrightL on 23.09.2014.
  */
 public class TASK018 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        double x = sc.nextDouble();
-        double l = x;
-        double sum = 0;
-        while (n > 1) {
-            x = x * l;
-            n--;
-        }
-        while (x != 1 / l) {
+        if (n == 0) {
             double a = sc.nextDouble();
-            sum = sum + a * x;
-            x = x / l;
+            System.out.println(a);
+            return;
         }
-        System.out.println(sum);
+        double x = sc.nextDouble();
+        double a = sc.nextDouble();
+        double s = a * x;
+        a = sc.nextDouble();
+        s = s + a;
+        for (int i = 1; i <= n - 1; i++) {
+            a = sc.nextDouble();
+            s = s * x + a;
+        }
+        System.out.println(s);
     }
 }
