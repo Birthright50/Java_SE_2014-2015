bbce75bf8dc57719e872c8dba946c0f981865688
Birthright <birthright5050@gmail.com>
2014-11-12 00:49:07 +0400

    task023 corrected

--- task/src/TASK023.java
+++ task/src/TASK023.java
@@ -6,15 +6,17 @@ public class TASK023 {
 
     public static void main(String[] args) {
         final double EPS = 1e-9;
-        double numerator = 5;
-        double denominator = 6;
-        double s = 0;
+        double numerator;
+        double denominator;
+        double s = 5.0/6;
+        System.out.println(s);
         double z = 1;
         for (int i = 2; z > EPS; i++) {
-            s = s + numerator / denominator;
-            z = numerator / denominator;
             numerator = 2 * i + 3;
             denominator = 5 * i * i * i * i + 1;
+            z = numerator / denominator;
+            s = s + z;
+            System.out.println(i);
         }
         System.out.println(s);
     }
