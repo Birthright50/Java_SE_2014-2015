14a21d8572f9fe13a1994b515a0a767d20c86e53
Birthright <birthright5050@gmail.com>
2014-11-12 01:29:17 +0400

    task048 corrected

---
+++ task048/Student.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * Created by BirthrightL on 11.11.2014.
+ */
+public class Student {
+    private String fio;
+    private int year;
+
+    public Student(String s, int y) {
+        fio = s;
+        year = y;
+    }
+
+    public static void studentInfo() {
+        System.out.println("Это всего лишь метод " +
+                "для демонстрации static");
+    }
+
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void greet(Student s) {
+        System.out.println(fio +
+                        " приветствует студента с именем " +
+                        s.getFio()
+        );
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setFio(String s) {
+        fio = s;
+    }
+
+    public void setYear(int y) {
+        year = y;
+    }
+}
