3b4cc299a7c68a34cb567259176a485d34f26e80
Birthright <birthright5050@gmail.com>
2014-11-22 18:37:11 +0400

    task028 corrected

--- task/src/TASK028.java
+++ task/src/TASK028.java
@@ -5,37 +5,19 @@ import java.util.Scanner;
  */
 public class TASK028 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ввод k");
         int k = sc.nextInt();
-        System.out.println("Ввод m");
         int m = sc.nextInt();
-        int b;
-        int n = 0;
-        int g;
-        if (k > m) {
-            b = k - m - 1;
-            g = m;
-            while (n < b) {
-                g = g + 1;
-                n++;
-                if (g % 3 == 0) {
-                    System.out.println(g);
-                }
-            }
+        if (k % 3 != 0) {
+            k += 3 - (k % 3);
         } else {
-            b = m - k - 1;
-            g = k;
-            while (n < b) {
-                g = g + 1;
-                n++;
-                if (g % 3 == 0) {
-                    System.out.println(g);
-
-                }
-            }
-
-
+            k += 3;
+        }
+        while (k < m) {
+            System.out.println(k);
+            k += 3;
         }
     }
 }
+
