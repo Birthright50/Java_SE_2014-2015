52cd17b60f5df4c78a0ca869b6cd1f2903bc3007
Birthright <birthright5050@gmail.com>
2014-11-22 16:45:11 +0400

    task030 corrected

--- task/src/TASK030.java
+++ task/src/TASK030.java
@@ -6,23 +6,20 @@ import java.util.Scanner;
 public class TASK030 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ввод размера массива");
         int n = sc.nextInt();
-        System.out.println("Ввод элементов массива");
-        int[] a = new int[n];
         int l;
         int chet = 0;
         int nechet = 0;
         boolean q = true;
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-            if ((a[i] >= 100 && a[i] <= 999) | (a[i] >= 10000 && a[i] < 99999) | (a[i] <= -100 && a[i] >= -999) | (a[i] <= -10000 && a[i] >= -99999)) {
+            int a = sc.nextInt();
+            if ((a >= 100 && a <= 999) | (a >= 10000 && a < 99999) | (a <= -100 && a >= -999) | (a <= -10000 && a >= -99999)) {
                 int j = 0;
                 int h = 0;
                 int g = 0;
-                while (a[i] > 0) {
-                    l = a[i] % 10;
-                    a[i] = a[i] / 10;
+                while (a > 0) {
+                    l = a % 10;
+                    a = a / 10;
                     j++;
                     if (l % 2 == 0) {
                         g++;
@@ -30,7 +27,7 @@ public class TASK030 {
                         h++;
                     }
                 }
-                if (j == g ) {
+                if (j == g) {
                     chet++;
                 }
                 if (j == h) {
@@ -38,7 +35,7 @@ public class TASK030 {
                 }
             }
         }
-        if ((chet < 2 && chet > 2) | (nechet <2 && nechet >2)) {
+        if ((chet < 2 && chet > 2) | (nechet < 2 && nechet > 2)) {
             q = false;
         }
         System.out.println(q);
