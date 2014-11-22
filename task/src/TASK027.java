89d680cb247f74f0facf8f6dba2f5079e1114e32
Birthright <birthright5050@gmail.com>
2014-11-22 16:40:08 +0400

    task027 corrected

--- task/src/TASK027.java
+++ task/src/TASK027.java
@@ -6,15 +6,12 @@ import java.util.Scanner;
 public class TASK027 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ввод размера массива");
         int n = sc.nextInt();
-        System.out.println("Ввод элементов массива");
-        int[] a = new int[n];
         boolean b = false;
-        for (int i = 0 ;!b && i<n; i++){
-            a[i]=sc.nextInt();
-            if ((a[i]%2==0 & a[i]%3==0) | (a[i]%5==0 & a[i]%6==0) ) {
-                b=true;
+        for (int i = 0; !b && i < n; i++) {
+            int a = sc.nextInt();
+            if ((a % 2 == 0 && a % 3 == 0) || (a % 5 == 0 && a % 6 == 0)) {
+                b = true;
             }
         }
         System.out.println(b);
