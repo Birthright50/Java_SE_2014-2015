69a9f036b7bf3e149fb9b572954c1de25f315c4f
Birthright <birthright5050@gmail.com>
2014-11-23 17:23:01 +0300

    task047 corrected

--- task/src/TASK047.java
+++ task/src/TASK047.java
@@ -4,53 +4,52 @@ import java.util.Scanner;
  * Created by BirthrightL on 02.11.2014.
  */
 public class TASK047 {
-    public static void TablUmn(int k) {
+    public static void multTable(int k) {
         for (int i = 1; i < 11; i++) {
             String l = i + " x " + k + " = " + (i * k);
             System.out.println(l);
         }
     }
 
-    public static void BeskSum(int x) {
+    public static void infSum(int x) {
         final double EPS = 1e-9;
         double denominator = 9 * (x - 1) * (x - 1);
-        double s = 0;
+        double s = 1 / denominator;
         double z = 1;
         if (x == 1)
             System.out.println("Infinity");
         else {
             for (int i = 2; z > EPS; i++) {
-                s = s + 1 / denominator;
-                z = 1 / denominator;
                 denominator = denominator * i * 9 * (x - 1) * (x - 1) / (i - 1);
+                z = 1 / denominator;
+                s = s + z;
             }
             System.out.println(s);
         }
     }
 
-    public static int ScalyarAB(int[] a, int[] b, int n) {
+    public static int scalarProduct(int[] a, int[] b) {
         int scal = 0;
-        for (int i = 0; i < n; i++) {
-            scal = scal + a[i] * b[i];
+        for (int i = 0; i < a.length; i++) {
+            scal += (a[i] * b[i]);
         }
         return scal;
     }
 
-    public static void Cos(int[] a, int[] b, int n, int q) {
+    public static double cos(int[] a, int[] b) {
         double vect1 = 0;
         double vect2 = 0;
-        for (int i = 0; i < n; i++) {
-
-            vect1 = vect1 + a[i] * a[i];
-            vect2 = vect2 + b[i] * b[i];
+        for (int i = 0; i < a.length; i++) {
+            vect1 += (a[i] * a[i]);
+            vect2 += (b[i] * b[i]);
         }
         vect1 = Math.pow(vect1, 0.5);
         vect2 = Math.pow(vect2, 0.5);
-        double cos = q / (vect1 * vect2);
-        System.out.println("Косинус = " + cos);
+        double cos = scalarProduct(a, b) / (vect1 * vect2);
+        return cos;
     }
 
-    public static double[][] Gauss(double[][] m, int z) {
+    public static double[][] gauss(double[][] m, int z) {
         for (int i = 0; i < z - 1; i++) {
             for (int j = 0; j < z - i - 1; j++) {
                 double l = m[j + i + 1][i] / m[i][i];
@@ -58,7 +57,8 @@ public class TASK047 {
                     m[j + i + 1][g + i] = m[j + i + 1][g + i] - l * m[i][g + i];
                 }
             }
-        } return m;
+        }
+        return m;
     }
 
 
@@ -66,12 +66,12 @@ public class TASK047 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ввод k (task008)");
         int k = scanner.nextInt();
-        TablUmn(k);
+        multTable(k);
 
 
         System.out.println("Ввод х (task024)");
         int x = scanner.nextInt();
-        BeskSum(x);
+        infSum(x);
 
 
         System.out.println("Ввод n (task033)");
@@ -86,9 +86,10 @@ public class TASK047 {
         for (int i = 0; i < n; i++) {
             b[i] = scanner.nextInt();
         }
-        int q = ScalyarAB(a, b, n);
+        int q = scalarProduct(a, b);
         System.out.println("Скалярное произведение = " + q);
-        Cos(a, b, n, q);
+        double l = cos(a, b);
+        System.out.println("Косинус = " + l);
 
         System.out.println("Ввод n (task038)");
         int z = scanner.nextInt();
@@ -96,10 +97,10 @@ public class TASK047 {
         double[][] m = new double[z][z];
         for (int i = 0; i < z; i++) {
             for (int j = 0; j < z; j++) {
-                m[i][j] = scanner.nextInt();
+                m[i][j] = scanner.nextDouble();
             }
         }
-        m = Gauss(m, z);
+        m = gauss(m, z);
         for (int i = 0; i < z; i++) {
             for (int j = 0; j < z; j++) {
                 System.out.print(m[i][j] + " ");
