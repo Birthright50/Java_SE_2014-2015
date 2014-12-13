2b44394ec279c9ef8b9cb82b796ef55ae5843a58
Birthright <birthright5050@gmail.com>
2014-12-13 23:08:01 +0300

    task034 corrected

--- task/src/TASK034.java
+++ task/src/TASK034.java
@@ -7,17 +7,20 @@ public class TASK034 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[]a=new int[n];
+        int[] a = new int[n];
 
-        for (int i = 0; i<n; i++){
-            a[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
-        int sum = a[0]+a[1]+a[2];
-        for (int i = 1; i<n-2; i++){
-            if(a[i]+a[i+1]+a[i+2]>sum){
-                sum=a[i]+a[i+1]+a[i+2];
+        long d = System.nanoTime();
+        int sum = a[0] + a[1] + a[2];
+        for (int i = 1; i < n - 2; i++) {
+            int l = a[i] + a[i + 1] + a[i + 2];
+            if (l > sum) {
+                sum = l;
             }
         }
+        System.out.println(System.nanoTime() - d);
         System.out.println(sum);
 
     }
