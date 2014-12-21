40d3b270a39edb67bc25648eda82663b2252a627
Birthright <birthright5050@gmail.com>
2014-12-22 01:56:45 +0300

    task046 corrected

--- task/src/TASK046.java
+++ task/src/TASK046.java
@@ -9,41 +9,19 @@ public class TASK046 {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
         String b = sc.nextLine();
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        if (a.equals(b)) {
-            System.out.println("Одинаковые строки");
-        }
-        int c = a.length();
-        int d = b.length();
-        int min;
-        if (c <= d) {
-            min = c;
-        } else {
-            min = d;
-        }
-        char e[] = a.toCharArray();
-        char f[] = b.toCharArray();
-        for (int i = 0; i < min; i++) {
-            char c1 = e[i];
-            char c2 = f[i];
-            if (c1 < c2) {
-                System.out.println("Первая");
-                return;
+        int i = 0;
+        while (i < a.length()) {
+            if (a.charAt(i) != b.charAt(i)) {
+                if ((int) a.charAt(i) < (int) b.charAt(i)) {
+                    System.out.println("1-я строка лексикографически раньше ");
+                    break;
+                } else {
+                    System.out.println("2-я строка лексикографически раньше ");
+                    break;
+                }
             }
-            if (c1 > c2) {
-                System.out.println("Вторая");
-                return;
-            }
+            i++;
         }
-        if (c <= d && a.equals(b.substring(0, min))){
-            System.out.println("Первая");
-        }
-        if (d < c && b.equals(a.substring(0, min))){
-            System.out.println("Вторая");
-        }
-
-
     }
 }
 
