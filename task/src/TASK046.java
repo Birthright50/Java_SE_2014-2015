ec0a01ded791062eb8d3f0884e83a5a71322348a
Birthright <birthright5050@gmail.com>
2014-12-22 02:13:22 +0300

    task046 corrected

--- task/src/TASK046.java
+++ task/src/TASK046.java
@@ -10,17 +10,24 @@ public class TASK046 {
         String a = sc.nextLine();
         String b = sc.nextLine();
         int i = 0;
-        while (i < a.length()) {
+        int l = Math.min(a.length(), b.length());
+        while (i < l) {
             if (a.charAt(i) != b.charAt(i)) {
                 if ((int) a.charAt(i) < (int) b.charAt(i)) {
-                    System.out.println("1-я строка лексикографически раньше ");
-                    break;
+                    System.out.println("1-я строка лексикографически раньше");
+                    return;
                 } else {
-                    System.out.println("2-я строка лексикографически раньше ");
-                    break;
+                    System.out.println("2-я строка лексикографически раньше");
+                    return;
                 }
             }
             i++;
+        }
+        if(a.length()-b.length()<0){
+            System.out.println("1-я строка лексикографически раньше");
+        }
+        else{
+            System.out.println("2-я строка лексикографически раньше");
         }
     }
 }
