7235a475effe70819e036a0e461e1c520c3bda8d
Birthright <birthright5050@gmail.com>
2016-08-08 16:57:54 +0000

    TASK018.java edited online with Bitbucket

--- FirstYear/task/src/TASK018.java
+++ FirstYear/task/src/TASK018.java
@@ -7,17 +7,10 @@ public class TASK018 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n == 0) {
-            double a = sc.nextDouble();
-            System.out.println(a);
-            return;
-        }
         double x = sc.nextDouble();
-        double a = sc.nextDouble();
-        double s = a * x;
-        a = sc.nextDouble();
-        s = s + a;
-        for (int i = 1; i <= n - 1; i++) {
+        double a;
+        double s = 0.0;
+        for (int i = 0; i <= n; i++) {
             a = sc.nextDouble();
             s = s * x + a;
         }
